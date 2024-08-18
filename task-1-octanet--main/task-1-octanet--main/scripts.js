30a51d894be04a1d7fe4f9eeb77f7a7d78b09926
sarosaravanan1 <144226684+sarosaravanan1@users.noreply.github.com>
2024-08-18 12:13:57 +0530

    Add files via upload

---
+++ task-1-octanet--main/task-1-octanet--main/scripts.js
@@ -0,0 +1,6 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting us!');
+
+    // Here you can add code to handle the form submission, e.g., send data to a server
+});
